25c65d1027aeed4fd09908ef68d0058265b8f5a9
Marcel <elbonian@gmail.com>
2020-07-07 22:27:27 -0700

    clahe() now returns an Image

--- clahe_par.py
+++ clahe_par.py
@@ -3,11 +3,12 @@ import numpy as np
 import timeit
 import collections
 import multiprocessing as mp
+import ray
+
+
 
 def clahe(image, blockRadius, bins, slope):
 
-    dest_image = image.copy()
-    dest_pix = dest_image.load()
     blockRadius = int((blockRadius-1)/2)
     bins = int(bins-1)
     slope = float(slope)
@@ -16,31 +17,39 @@ def clahe(image, blockRadius, bins, slope):
     boxXMax = box_x + box_width
     boxYMax = box_y + box_height
     np_pix = np.array(image)
-    pix = list(image.load())
+    pix = image.load()
+    new_image = image.copy()
+    new_pix = new_image.load()
 
     y = box_y
-#    clahe_rows(pix, dest_pix, y, boxYMax, blockRadius, bins, slope, box_width, box_height, box_x, boxXMax)
+
+    dest_rows = clahe_rows(pix, y, boxYMax, blockRadius, bins, slope, box_width, box_height, box_x, boxXMax)
 #    while y < boxYMax:
-#        clahe_row(pix, dest_pix, y, blockRadius, bins, slope, box_width, box_height, box_x, boxXMax)
+#        clahe_row(pix, y, blockRadius, bins, slope, box_width, box_height, box_x, boxXMax)
 #        y = y + 1
 
-    jobs = []
-    
-    p = mp.Process(target=job1, args=(pix, dest_pix, y, boxYMax, blockRadius, bins, slope, box_width, box_height, box_x, boxXMax,))
-    jobs.append(p)
-    p.start()
-    p.join()
-
-    dest_image.save("images/output.png")
-
-def job1(pix, dest_pix, y, boxYMax, blockRadius, bins, slope, box_width, box_height, box_x, boxXMax):
-    clahe_rows(pix, dest_pix, y, boxYMax, blockRadius, bins, slope, box_width, box_height, box_x, boxXMax)
-
-def clahe_rows(pix, dest_pix, y_start, y_end, blockRadius, bins, slope, box_width, box_height, box_x, boxXMax):
-    y = y_start
-    while y < y_end:
-        clahe_row(pix, dest_pix, y, blockRadius, bins, slope, box_width, box_height, box_x, boxXMax)
+    y = 0
+    while y<box_height:
+        x = 0
+        while x<box_width:
+            val = dest_rows[y][x]
+            new_pix[x, y] = val
+            x = x + 1
         y = y + 1
+
+    return new_image
+
+def job1(pix, y, boxYMax, blockRadius, bins, slope, box_width, box_height, box_x, boxXMax):
+    pix = pix.load()
+    clahe_rows(pix, y, boxYMax, blockRadius, bins, slope, box_width, box_height, box_x, boxXMax)
+
+def clahe_rows(pix, y_start, y_end, blockRadius, bins, slope, box_width, box_height, box_x, boxXMax):
+    y = y_start
+    dest_rows = []
+    while y < y_end:
+        dest_rows.append(clahe_row(pix, y, blockRadius, bins, slope, box_width, box_height, box_x, boxXMax))
+        y = y + 1
+    return dest_rows
 
 def roundPositive(num):
     return int(num + 0.5)
@@ -116,7 +125,7 @@ def cdf_values(v, clippedHist, bins):
 
     return cdf, cdfMax, cdfMin
 
-def clahe_row(pix, dest_pix, y, blockRadius, bins, slope, box_width, box_height, box_x, boxXMax):
+def clahe_row(pix, y, blockRadius, bins, slope, box_width, box_height, box_x, boxXMax):
     yMin = max(0, y - blockRadius)
     yMax = min(box_height, y + blockRadius + 1)
     h = yMax - yMin
@@ -125,6 +134,7 @@ def clahe_row(pix, dest_pix, y, blockRadius, bins, slope, box_width, box_height,
     xMax0 = min(box_width - 1, box_x + blockRadius)
 
     hist = histogram(xMin0, xMax0, yMin, yMax, pix, bins)
+    dest = []
 
     x = box_x
     while x < boxXMax:
@@ -157,5 +167,7 @@ def clahe_row(pix, dest_pix, y, blockRadius, bins, slope, box_width, box_height,
         cdf, cdfMax, cdfMin = cdf_values(v, clippedHist, bins)
         col = roundPositive((cdf - cdfMin) / (cdfMax - cdfMin) * 255)
 
-        dest_pix[x, y] = col
+        dest.append(col)
         x = x + 1
+    
+    return dest
--- clahe_test.py
+++ clahe_test.py
@@ -2,8 +2,14 @@ from clahe_par import clahe
 from PIL import Image
 import cProfile, pstats
 
+pr = cProfile.Profile()
+pr.enable()
+
 im = Image.open("images/snow_gray8_small.tif")
-cProfile.run("clahe(im, 127, 64, 3.0)","{}.profile".format(__file__))
-s = pstats.Stats("{}.profile".format(__file__))
-s.strip_dirs()
-s.sort_stats("time").print_stats(10)
+new_image = clahe(im, 127, 64, 3.0)
+new_image.save("images/output.png")
+
+pr.disable()
+ps = pstats.Stats(pr)
+ps.strip_dirs()
+ps.sort_stats("time").print_stats(10)
